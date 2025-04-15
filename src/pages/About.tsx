88b5572e865033c3fd0568f3b92b54d8d287a20c
cesar <cesarhernanruscica@gmail.com>
2025-04-15 12:20:30 -0300

    fix error - change name CampingStore.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { Box, Heading, Spinner, Text, VStack, Image } from '@chakra-ui/react';
 import MainLayout from '@/layouts/MainLayout';
-import { useCampingStore } from '@/store/campingStore';
+import { useCampingStore } from '@/store/CampingStore';
 import campingImage from '@/assets/camping/camping1.jpg';
-import { CampingType } from '@/store/campingStore';
+import { CampingType } from '@/store/CampingStore';
 
 function About() {
   const { campings, isLoading, error, fetchCampings  } = useCampingStore();
